652c481cc77e9c7f7dfe3192731c81fb7e2241b0
ellieraya <ellieraya@gmail.com>
2017-12-11 17:23:05 -0500

    styling

---
+++ src/components/todoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './todoItem.css';
+
+export default class TodoItem extends React.Component {
+  constructor(props) {
+    super (props);
+  }
+
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+
+  render(){
+    return(
+      <div className="todoWrapper">
+        <p>{this.props.todo.text}</p><button className="removeTodo" onClick={(e)=> this.removeTodo(this.props.id)}>remove</button>
+      </div>
+    )
+  }
+}
